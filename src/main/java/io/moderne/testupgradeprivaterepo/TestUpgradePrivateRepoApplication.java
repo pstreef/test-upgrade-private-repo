468e807332cab904ce2b4657358182f2f17f653d
Peter Streef <p.streef@gmail.com>
2024-08-15 10:25:08 +0200

    initial

---
+++ src/main/java/io/moderne/testupgradeprivaterepo/TestUpgradePrivateRepoApplication.java
@@ -0,0 +1,13 @@
+package io.moderne.testupgradeprivaterepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUpgradePrivateRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUpgradePrivateRepoApplication.class, args);
+    }
+
+}
